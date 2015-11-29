60544c7be4df2b8cf6a67d30e8758616e8b0f366
emergingmed-jshin <justin@justin-imac.local>
2015-11-29 03:01:00 -0500

    Init

---
+++ onecut/onecut/Sources/User Interface/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  onecut
+//
+//  Created by Justin Shin on 11/29/15.
+//  Copyright © 2015 jshin47. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ onecut/onecutTests/onecutTests.swift
@@ -0,0 +1,36 @@
+//
+//  onecutTests.swift
+//  onecutTests
+//
+//  Created by Justin Shin on 11/29/15.
+//  Copyright © 2015 jshin47. All rights reserved.
+//
+
+import XCTest
+@testable import onecut
+
+class onecutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
